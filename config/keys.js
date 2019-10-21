02738cbb247c3fd59f94e3177227485eb2c61e8f
Ricardo Mateus <r.fmateus@hotmail.com>
2019-10-22 00:54:19 +0100

    Handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Ricky:ricky@cluster0-wiix7.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID: '291874658140-52gg59cjpj3476v5808kaav7pp1c3hck.apps.googleusercontent.com',
+  googleClientSecret: 'KvX5TJ_g3rhJqslE9VX3icAr'
+}
